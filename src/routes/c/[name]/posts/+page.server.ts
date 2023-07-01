e4d0f985c7b0946f7710f9f7d6edde72888378a3
Xyphyn <james@xylight.dev>
2023-07-01 12:08:33 -0700

    feat: Add community sorting

--- src/routes/c/[name]/posts/+page.server.ts
+++ src/routes/c/[name]/posts/+page.server.ts
@@ -1,5 +1,16 @@
-import { lemmy } from '$lib/lemmy.js';
+import { lemmy } from '$lib/lemmy.js'
+import type { ListingType, SortType } from 'lemmy-js-client'
 
-export function load({ params }) {
-    return lemmy.getPosts({ limit: 20, community_name: params.name, page: 1, })
+export function load(req) {
+  const page = Number(req.url.searchParams.get('page') || 1) || 1
+
+  const sort: SortType =
+    (req.url.searchParams.get('sort') as SortType) || 'Active'
+
+  return lemmy.getPosts({
+    limit: 40,
+    community_name: req.params.name,
+    page: page,
+    sort: sort,
+  })
 }
--- src/routes/c/[name]/posts/+page.svelte
+++ src/routes/c/[name]/posts/+page.svelte
@@ -1,20 +1,53 @@
 <script lang="ts">
+  import { page } from '$app/stores'
+  import Link from '$lib/components/Link.svelte'
   import MultiSelect from '$lib/components/MultiSelect.svelte'
+  import SvelteMarkdown from 'svelte-markdown'
   import Avatar from '../../../post/[id]/Avatar.svelte'
   import Post from '../../../post/[id]/Post.svelte'
+  import { goto } from '$app/navigation'
 
   export let data
 </script>
 
 <div class="flex flex-row gap-4 justify-between">
   <div class="flex flex-col gap-4">
+    <div class="flex flex-col sm:flex-row gap-4">
+      <MultiSelect
+        options={['Active', 'TopDay', 'New']}
+        optionNames={['Active', 'Top (Day)', 'New']}
+        selected={$page.url.searchParams.get('sort') ?? 'Active'}
+        on:select={(e) => {
+          const url = $page.url
+          url.searchParams.set('sort', e.detail)
+          url.searchParams.delete('page')
+          goto(url, {
+            invalidateAll: true,
+          })
+        }}
+      />
+    </div>
     {#each data.posts as post}
       <Post {post} />
     {/each}
+    <div>
+      <Link
+        href={(function () {
+          $page.url.searchParams.set(
+            'page',
+            ((Number($page.url.searchParams.get('page')) || 1) + 1).toString()
+          )
+
+          return $page.url.toString()
+        })()}
+      >
+        Next
+      </Link>
+    </div>
   </div>
   <div
     class="bg-white border border-slate-200 dark:border-zinc-800
-  dark:bg-zinc-900 flex flex-col gap-4 p-8 max-w-sm rounded-md h-max"
+  dark:bg-zinc-900 hidden lg:flex flex-col gap-4 p-8 max-w-sm rounded-md h-max"
   >
     <Avatar
       width={48}
@@ -23,6 +56,6 @@
       alt={data.community_view.community.name}
     />
     <h1 class="font-bold text-lg">{data.community_view.community.name}</h1>
-    <p>{data.community_view.community.description}</p>
+    <SvelteMarkdown source={data.community_view.community.description} />
   </div>
 </div>
