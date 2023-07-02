06c39a17ac53581078bb0ee99fddcc73786fb15f
Xyphyn <james@xylight.dev>
2023-07-02 15:04:28 -0700

    misc: Update default instance

--- src/lib/lemmy.ts
+++ src/lib/lemmy.ts
@@ -2,8 +2,8 @@ import { LemmyHttp, type LoginResponse } from 'lemmy-js-client'
 import { writable } from 'svelte/store'
 import { PUBLIC_PROXY_URL } from '$env/static/public'
 
-export let instance_url = 'programming.dev'
-export function getClient(instance: string = `programming.dev`): LemmyHttp {
+export let instance_url = 'lemmy.world'
+export function getClient(instance: string = `lemmy.world`): LemmyHttp {
   return new LemmyHttp(`${PUBLIC_PROXY_URL}/cors/${instance}`)
 }
 
