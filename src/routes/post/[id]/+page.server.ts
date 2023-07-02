ccde8597273bdffe300133100a7968cb05acad35
Xyphyn <james@xylight.dev>
2023-07-01 19:04:23 -0700

    misc: Change max depth

--- src/routes/post/[id]/+page.server.ts
+++ src/routes/post/[id]/+page.server.ts
@@ -1,16 +1,16 @@
 import { lemmy } from '$lib/lemmy.js'
 
 export async function load({ params }) {
-    return {
-        post: await lemmy.getPost({ id: Number(params.id) }),
-        comments: await lemmy.getComments({
-            post_id: Number(params.id),
-            type_: 'All',
-            limit: 20,
-            page: 1,
-            max_depth: 3,
-            saved_only: false,
-            sort: 'Hot',
-        }),
-    }
+  return {
+    post: await lemmy.getPost({ id: Number(params.id) }),
+    comments: await lemmy.getComments({
+      post_id: Number(params.id),
+      type_: 'All',
+      limit: 20,
+      page: 1,
+      max_depth: 6,
+      saved_only: false,
+      sort: 'Hot',
+    }),
+  }
 }
