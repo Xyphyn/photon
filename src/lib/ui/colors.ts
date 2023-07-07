2ece9bd5c1ac10175b277cd58523453fc15c4397
Xyphyn <james@xylight.dev>
2023-07-07 14:58:23 -0700

    fix: Fix hover color

--- src/lib/ui/colors.ts
+++ src/lib/ui/colors.ts
@@ -9,5 +9,5 @@ export enum Color {
   'secondary' = 'hover:bg-black/10 hover:dark:bg-white/10 text-black dark:text-white fill-black dark:fill-white',
   'danger' = 'border border-red-500 bg-red-500 text-white hover:text-red-500 hover:bg-transparent',
   'dangerSecondary' = 'hover:bg-red-500 text-red-500 hover:text-white',
-  'border' = 'border border-slate-200 dark:border-zinc-800 bg-slate-100 dark:bg-zinc-900 hover:bg-slate-200 hover:dark:bg-slate-800',
+  'border' = 'border border-slate-200 dark:border-zinc-800 bg-slate-100 dark:bg-zinc-900 hover:bg-slate-200 hover:dark:bg-zinc-800',
 }
