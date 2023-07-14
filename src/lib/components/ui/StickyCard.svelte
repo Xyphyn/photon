8f2e322de161b253266df2ea9364da617857ac7e
Xyphyn <james@xylight.dev>
2023-07-14 12:36:42 -0700

    misc: Update semantics

--- src/lib/components/ui/StickyCard.svelte
+++ src/lib/components/ui/StickyCard.svelte
@@ -1,8 +1,8 @@
-<div
+<aside
   class="sticky top-20 border bg-white shadow-md border-slate-100
     dark:border-zinc-800 dark:bg-zinc-900 break-words flex
     flex-col gap-4 p-8 w-[20rem] rounded-md h-max max-h-[calc(100vh-6rem)]
     overflow-auto"
 >
   <slot />
-</div>
+</aside>
