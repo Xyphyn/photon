f127b5f64594593fca8ac9bb6871504a62b4970c
Xyphyn <james@xylight.dev>
2023-07-16 16:44:59 -0700

    fix: remove border

--- src/lib/components/lemmy/Post.svelte
+++ src/lib/components/lemmy/Post.svelte
@@ -109,7 +109,7 @@
       />
       <span
         class="w-full px-4 py-2 overflow-hidden
-        whitespace-nowrap text-ellipsis text-sm group-hover:underline bg-slate-100 dark:bg-slate-800 border
+        whitespace-nowrap text-ellipsis text-sm group-hover:underline bg-slate-100 dark:bg-zinc-800 border dark:border-transparent
         absolute bottom-0 rounded-b-md flex flex-row gap-1 items-center"
       >
         <Icon src={Link} width={16} mini />
