9fb5460266d8ca28218e5784b82238c1d82dd3bc
Peter Bui <pbui@nd.edu>
2023-07-26 09:30:17 -0400

    fix: allow UI settings to persist
    UI settings were being overwritten on refresh. Changing this check
    appears to resolve this issue and allows for the UI settings to persist.

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -27,21 +27,12 @@ const defaultSettings: Settings = {
 
 export const userSettings = writable(defaultSettings)
 
-function mergeObjects<T>(obj1: any, obj2: any): T {
-  for (let prop in obj2) {
-    if (!(prop in obj1) || obj1[prop] == null || obj2[prop] == '') {
-      obj1[prop] = obj2[prop]
-    }
-  }
-  return obj1
-}
-
 if (typeof window != 'undefined') {
   let oldUserSettings = JSON.parse(
     localStorage.getItem('settings') ?? JSON.stringify(defaultSettings)
   )
 
-  userSettings.set(mergeObjects<Settings>(oldUserSettings, defaultSettings))
+  userSettings.set({...defaultSettings, ...oldUserSettings})
 }
 
 userSettings.subscribe((settings) => {
