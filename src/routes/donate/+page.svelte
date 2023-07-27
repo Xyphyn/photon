d427d4b2fcfa610563596968131b90c3673d2549
Xyphyn <james@xylight.dev>
2023-07-27 11:58:17 -0700

    misc: remove useless page

--- src/routes/donate/+page.svelte
+++
@@ -1,6 +0,0 @@
-<h1 class="font-bold text-4xl">I don't take donations.</h1>
-
-<p>
-  This is a project that I'm making because it's fun, not to gain money. If you
-  want to support me, contribute! Report bugs, fix typos, etc.
-</p>
