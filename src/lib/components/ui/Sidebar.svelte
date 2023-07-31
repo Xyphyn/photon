30b6daf4c77d35421595377747edb8ecc769e493
Xyphyn <james@xylight.dev>
2023-07-31 11:35:26 -0700

    fix: fix black sidebar

--- src/lib/components/ui/Sidebar.svelte
+++ src/lib/components/ui/Sidebar.svelte
@@ -1,6 +1,6 @@
 <nav
   class="hidden xl:flex flex-col pl-4 pr-4 py-4 overflow-auto sticky top-16 bottom-0
-      flex-1 gap-1 max-h-[calc(100vh-4rem)] w-full bg-black"
+      flex-1 gap-1 max-h-[calc(100vh-4rem)] w-full bg-slate-100 dark:bg-black"
 >
   <slot />
 </nav>
