292e3b823835990f3e328d0a49567fd8912de1c8
Xyphyn <james@xylight.dev>
2023-08-06 13:13:12 -0700

    fix: modal going out of window

--- src/lib/components/ui/StickyCard.svelte
+++ src/lib/components/ui/StickyCard.svelte
@@ -1,6 +1,6 @@
 <aside
   class="sticky top-16 break-words flex
-    flex-col gap-4 p-6 w-96 max-h-[calc(100svh-4rem)]
+    flex-col gap-4 p-6 max-w-full w-96 max-h-[calc(100svh-4rem)]
     overflow-auto text-sm border-slate-200 dark:border-zinc-800 {$$props.class}"
 >
   <slot />
--- src/lib/components/ui/modal/Modal.svelte
+++ src/lib/components/ui/modal/Modal.svelte
@@ -15,8 +15,8 @@
 {#if open}
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <div
-    class="overflow-hidden fixed top-0 left-0 w-screen h-screen z-[100]
-flex flex-col items-center justify-center bg-black/50 backdrop-blur-sm"
+    class="overflow-hidden fixed top-0 left-0 w-screen h-screen z-50
+flex flex-col items-center justify-center bg-black/50 backdrop-blur-sm box-border p-4"
     transition:fade={{ duration: 200 }}
     on:click={() => (open = false)}
   >
@@ -24,12 +24,12 @@ flex flex-col items-center justify-center bg-black/50 backdrop-blur-sm"
     <div
       on:click|stopPropagation={() => {}}
       transition:scale={{ start: 0.9, easing: expoOut }}
-      class="overflow-y-auto {$$props.class} my-4 rounded-xl"
+      class="overflow-y-auto {$$props.class} rounded-xl max-w-full box-border"
     >
-      <Card class="w-[calc(100vw-1rem)] dark:!bg-zinc-950 rounded-xl max-w-xl">
-        <div class="p-6 flex flex-col gap-4">
-          <div class="flex flex-row">
-            <h1 class="font-bold text-2xl w-max">
+      <Card class="w-full dark:!bg-zinc-950 rounded-xl max-w-xl box-border">
+        <div class="p-6 flex flex-col gap-4 w-full">
+          <div class="flex flex-row max-w-full">
+            <h1 class="font-bold text-2xl w-max max-w-full">
               <slot name="title" />
             </h1>
             <Button class="!p-2 ml-auto" on:click={() => (open = false)}>
