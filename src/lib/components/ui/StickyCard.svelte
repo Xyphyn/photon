4f898915b26b7446fdcea71f3d0fab893c7045f6
Peter Bui <pbui@nd.edu>
2023-08-05 21:51:05 -0400

    misc: allow community card to be full width and remove left border (#72)
    Right now, the community card model displays the information in a very
    narrow column with a lot of extra whitespace on the side and a weird
    left border.  Likewise, user pages also include a left border if the
    StickyCard is placed at the top.
    
    This removes the left border completely, and allows the StickyCard to be
    wider in order to allow for more information to be displayed.

--- src/lib/components/ui/StickyCard.svelte
+++ src/lib/components/ui/StickyCard.svelte
@@ -1,7 +1,7 @@
 <aside
   class="sticky top-16 break-words flex
-    flex-col gap-4 p-6 max-w-[20rem] min-w-[18rem] max-h-[calc(100svh-4rem)]
-    overflow-auto text-sm border-l border-slate-200 dark:border-zinc-800 {$$props.class}"
+    flex-col gap-4 p-6 max-w-[32rem] min-w-[18rem] max-h-[calc(100svh-4rem)]
+    overflow-auto text-sm border-slate-200 dark:border-zinc-800 {$$props.class}"
 >
   <slot />
 </aside>
