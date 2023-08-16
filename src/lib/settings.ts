61a8344fd717f867b1b093dfe61772e0c2869b16
Xyphyn <james@xylight.dev>
2023-08-16 15:45:52 -0700

    hotfix: fix typescript errors

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -2,13 +2,15 @@ import type { CommentSortType, SortType } from 'lemmy-js-client'
 import { writable } from 'svelte/store'
 import { env } from '$env/dynamic/public'
 
-console.log("Using the following default settings from the environment:");
-console.log(env);
+console.log('Using the following default settings from the environment:')
+console.log(env)
 
 // Returns a proper boolean or null.  Used to set boolean values from env var strings while allowing nullish coalescing to set default values.
-const toBool = (str) => {
-  if (typeof str === "undefined") { return null; }
-  return str === "true";
+const toBool = (str: string | undefined) => {
+  if (!str) {
+    return null
+  }
+  return str.toLowerCase() === 'true'
 }
 
 interface Settings {
@@ -49,39 +51,38 @@ interface Settings {
 }
 
 export const defaultSettings: Settings = {
-
   newComments: true,
-  expandableImages: toBool(env.PUBLIC_EXPANDABLE_IMAGES)	?? true,
-  markReadPosts: toBool(env.PUBLIC_MARK_READ_POSTS)		?? true,
-  revertColors: toBool(env.PUBLIC_REVERT_VOTE_COLORS)		?? false,
+  expandableImages: toBool(env.PUBLIC_EXPANDABLE_IMAGES) ?? true,
+  markReadPosts: toBool(env.PUBLIC_MARK_READ_POSTS) ?? true,
+  revertColors: toBool(env.PUBLIC_REVERT_VOTE_COLORS) ?? false,
   showInstances: {
-    user: 	toBool(env.PUBLIC_SHOW_INSTANCES_USER) 		?? false,
-    community: 	toBool(env.PUBLIC_SHOW_INSTANCES_COMMUNITY) 	?? true,
-    comments: 	toBool(env.PUBLIC_SHOW_INSTANCES_COMMENTS)	?? false,
+    user: toBool(env.PUBLIC_SHOW_INSTANCES_USER) ?? false,
+    community: toBool(env.PUBLIC_SHOW_INSTANCES_COMMUNITY) ?? true,
+    comments: toBool(env.PUBLIC_SHOW_INSTANCES_COMMENTS) ?? false,
   },
-  showCompactPosts: toBool(env.PUBLIC_SHOW_COMPACT_POSTS) 	?? false,
+  showCompactPosts: toBool(env.PUBLIC_SHOW_COMPACT_POSTS) ?? false,
   defaultSort: {
-    sort: 	env.PUBLIC_DEFAULT_FEED_SORT 			?? 'Active',
-    feed: 	env.PUBLIC_DEFAULT_FEED 			?? 'Local',
-    comments: 	env.PUBLIC_DEFAULT_COMMENT_SORT			?? 'Hot',
+    sort: env.PUBLIC_DEFAULT_FEED_SORT ?? ('Active' as any),
+    feed: env.PUBLIC_DEFAULT_FEED ?? ('Local' as any),
+    comments: env.PUBLIC_DEFAULT_COMMENT_SORT ?? ('Hot' as any),
   },
   hidePosts: {
-    deleted: 	toBool(env.PUBLIC_HIDE_DELETED) 		?? true,
-    removed: 	toBool(env.PUBLIC_HIDE_REMOVED) 		?? false,
+    deleted: toBool(env.PUBLIC_HIDE_DELETED) ?? true,
+    removed: toBool(env.PUBLIC_HIDE_REMOVED) ?? false,
   },
-  fullWidthLayout: toBool(env.PUBLIC_FULL_WIDTH_LAYOUT) 	?? false,
-  expandSidebar: toBool(env.PUBLIC_EXPAND_SIDEBAR) 		?? true,
+  fullWidthLayout: toBool(env.PUBLIC_FULL_WIDTH_LAYOUT) ?? false,
+  expandSidebar: toBool(env.PUBLIC_EXPAND_SIDEBAR) ?? true,
   notifications: {
     enabled: false,
     pollRate: 60 * 1000,
     notifRate: 10 * 60 * 1000,
   },
-  displayNames: toBool(env.PUBLIC_DISPLAY_NAMES) 		?? true,
-  nsfwBlur: toBool(env.PUBLIC_NSFW_BLUR)			?? true,
+  displayNames: toBool(env.PUBLIC_DISPLAY_NAMES) ?? true,
+  nsfwBlur: toBool(env.PUBLIC_NSFW_BLUR) ?? true,
   moderation: {
     removalReasonPreset: `Your submission in *"{{post}}"* was removed for {{reason}}.`,
   },
-  newVote: toBool(env.PUBLIC_NEW_VOTE_BUTTONS)			?? false,
+  newVote: toBool(env.PUBLIC_NEW_VOTE_BUTTONS) ?? false,
 }
 
 export const userSettings = writable(defaultSettings)
