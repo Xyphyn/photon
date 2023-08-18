dcf1fba1dfb98775d66b726e70762b432164860f
Xyphyn <james@xylight.dev>
2023-08-18 08:04:52 -0700

    misc: fix link

--- src/lib/components/input/Link.svelte
+++ src/lib/components/input/Link.svelte
@@ -3,7 +3,6 @@
   export let highlight: boolean = false
   export let nowrap: boolean = false
 
-  $: url = new URL(href)
 </script>
 
 <a {href} class="{highlight ? 'text-blue-500' : ''} hover:underline max-w-full">
@@ -14,15 +13,7 @@
         ? 'w-full whitespace-nowrap overflow-hidden text-ellipsis text-xs'
         : ''} {$$props.class}"
     >
-      <span class="opacity-90">
-        {url.protocol}//
-      </span>
-      <span class="font-bold">
-        {url.hostname}
-      </span>
-      <span class="opacity-90">
-        {url.pathname}
-      </span>
+    {href}
     </span>
   </slot>
 </a>
