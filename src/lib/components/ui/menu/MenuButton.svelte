831cc72ef7ccbd20bfbe8391f6354a0c164739ad
Xyphyn <james@xylight.dev>
2023-08-27 12:39:36 -0700

    fix: change colors

--- src/lib/components/ui/menu/MenuButton.svelte
+++ src/lib/components/ui/menu/MenuButton.svelte
@@ -9,9 +9,10 @@
     secondary:
       'hover:bg-neutral-200/50 hover:dark:bg-zinc-600/20 text-slate-800 fill-slate-800 dark:text-zinc-200 dark:fill-zinc-200',
     dangerSecondary: 'hover:bg-red-500 text-red-500 hover:text-white',
-    success: 'hover:bg-green-600 text-green-500 hover:text-white',
+    success:
+      'hover:bg-green-600 text-green-600 dark:text-green-500 hover:text-white hover:dark:text-white',
     warning:
-      'hover:bg-yellow-500 dark:text-yellow-300 text-yellow-500 hover:text-black',
+      'hover:bg-yellow-500 dark:text-yellow-300 text-yellow-600 hover:dark:text-black hover:text-black',
   }
 
   export let color: ButtonColor = 'secondary'
