b3ad7eeae90a31bc70959f033fbb42e73657f3eb
Xylight <xylight@xylight.dev>
2023-11-27 17:33:41 -0800

    fix: broken regex

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -106,7 +106,7 @@ export const removeItem = <T>(array: T[], predicate: (item: T) => boolean) => {
 export const DOMAIN_REGEX =
   /^(http(s)?:\/\/)?((?!-)[A-Za-z0-9-]{1,63}\.)+[A-Za-z]{2,63}(:[0-9]{0,5})$/g
 export const DOMAIN_REGEX_FORMS =
-  '(http(s)?://)?((?!-)[A-Za-z0-9-]{1,63}.)+[A-Za-z]{2,63}(:[0-9]{0,5})?'
+  '(http(s)?://)?((?!-)[A-Za-z0-9]{1,63}.)+[A-Za-z]{2,63}(:[0-9]{0,5})?'
 
 export const isSubscribed = (subscribed: SubscribedType) =>
   subscribed == 'Pending' || subscribed == 'Subscribed'
