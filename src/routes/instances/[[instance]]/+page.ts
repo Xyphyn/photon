9e9f6fb30aad7f18fbb43d06326729ec349b0d0d
CmdrKeen <0xCmdrKeen@pm.me>
2023-11-29 22:41:11 -0800

    Add optional instance route parameter to /instances

--- src/routes/instances/[[instance]]/+page.ts
+++ src/routes/instances/[[instance]]/+page.ts
@@ -2,7 +2,7 @@ import { profile } from '$lib/auth.js'
 import { getClient } from '$lib/lemmy.js'
 import { get } from 'svelte/store'
 
-export async function load({ fetch }) {
-  return (await getClient(undefined, fetch).getFederatedInstances())
+export async function load({ params, fetch }) {
+  return (await getClient(params.instance, fetch).getFederatedInstances())
     .federated_instances
 }
