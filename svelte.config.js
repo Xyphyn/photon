7cb7b800605573847ffd39d4d30720841856aa10
Xylight <james@xylight.dev>
2024-02-10 15:21:55 -0800

    fix: remove adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import auto from '@sveltejs/adapter-auto';
 import node from '@sveltejs/adapter-node';
 import bun from '@catdadcode/svelte-adapter-bun'
+import vercel from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: process.env.ADAPTER == 'node' ? node() : process.env.ADAPTER == 'bun' ? bun() : auto(),
+		adapter: process.env.ADAPTER == 'node' ? node() : process.env.ADAPTER == 'bun' ? bun() : vercel(),
 		// csp: {
 		// 	directives: {
 		// 		'script-src': ['self'],
