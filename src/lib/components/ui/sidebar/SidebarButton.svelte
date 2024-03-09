3f5c49d4c4fc39fd8fabc4b738ffde963fcd40c7
Xylight <james@xylight.dev>
2024-03-09 11:44:50 -0800

    fix: text-primary-100 wont work in sidebar

--- src/lib/components/ui/sidebar/SidebarButton.svelte
+++ src/lib/components/ui/sidebar/SidebarButton.svelte
@@ -11,7 +11,7 @@
   alignment="left"
   on:click
   class="hover:bg-slate-100 font-normal {selected
-    ? 'text-primary-900 dark:text-primary-100'
+    ? 'text-primary-900 dark:!text-primary-100'
     : 'text-slate-600 dark:text-zinc-400'} {$userSettings.expandSidebar
     ? ''
     : 'max-lg:!p-1 [&>*]:max-lg:!justify-center'} {$$props.class}"
