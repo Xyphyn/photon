103b421b8c33724e9fae68d7abe1f1b35b895c55
Xylight <james@xylight.dev>
2024-03-09 11:01:55 -0800

    fix: clicking thumbnails sends you to main page in list

--- src/lib/components/lemmy/post/media/PostMediaCompact.svelte
+++ src/lib/components/lemmy/post/media/PostMediaCompact.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { bestImageURL, postLink } from '$lib/components/lemmy/post/helpers.js'
-  import ExpandableImage from '$lib/components/ui/ExpandableImage.svelte'
+  import { showImage } from '$lib/components/ui/ExpandableImage.svelte'
   import { userSettings } from '$lib/settings.js'
   import { isImage } from '$lib/ui/image'
   import type { Post } from 'lemmy-js-client'
@@ -21,14 +21,14 @@
         />
       </a>
     {:else}
-      <ExpandableImage url={bestImageURL(post, false, 2048)}>
+      <button on:click={() => showImage(bestImageURL(post, false, 2048))}>
         <!-- svelte-ignore a11y-missing-attribute -->
         <img
           src={bestImageURL(post, true)}
           loading="lazy"
           class="object-cover bg-slate-100 rounded-md h-24 w-24 border border-slate-200 dark:border-zinc-700"
         />
-      </ExpandableImage>
+      </button>
     {/if}
   </div>
 {/if}
