9dc1b70a4edf1a2d57a826c9863b191680433012
Xylight <james@xylight.dev>
2024-04-13 12:51:24 -0700

    fix: sidebar not going down all the way

--- src/lib/components/ui/sidebar/Sidebar.svelte
+++ src/lib/components/ui/sidebar/Sidebar.svelte
@@ -28,55 +28,10 @@
 </script>
 
 <nav
-  class="flex flex-col pl-4 pr-4 py-4 overflow-auto
-  gap-1 max-h-[calc(100svh-4rem)] {$$props.class}"
+  class="flex flex-col p-4 overflow-auto
+  gap-1 h-screen {$$props.class}"
   style={$$props.style}
 >
-  <Button
-    on:click={() =>
-      ($userSettings.expandSidebar = !$userSettings.expandSidebar)}
-    size="square-md"
-    class="flex-shrink-0 lg:hidden"
-  >
-    <Icon src={ChevronDoubleRight} size="16" mini />
-  </Button>
-  <SidebarButton href="/" selected={route == '/'}>
-    <Icon src={Home} solid={route == '/'} size="20" title="Frontpage" />
-    <span slot="label">Frontpage</span>
-  </SidebarButton>
-  <SidebarButton href="/settings" selected={route == '/settings'}>
-    <Icon
-      src={Cog6Tooth}
-      solid={route == '/settings'}
-      size="20"
-      title="Settings"
-    />
-    <span slot="label">Settings</span>
-  </SidebarButton>
-  <SidebarButton href="/search" selected={route == '/search'}>
-    <Icon solid={route == '/search'} src={MagnifyingGlass} size="20" />
-    <span slot="label">Search</span>
-  </SidebarButton>
-  <SidebarButton href="/communities" selected={route == '/communities'}>
-    <Icon solid={route == '/communities'} src={GlobeAlt} size="20" />
-    <span slot="label">Communities</span>
-  </SidebarButton>
-  {#if $profile?.jwt}
-    <SidebarButton href="/inbox" selected={route == '/inbox'}>
-      <Icon solid={route == '/inbox'} src={Inbox} size="20" />
-      <span slot="label" class="inline-flex items-center flex-1">
-        <span class="flex-shrink-0 flex-1">Inbox</span>
-        {#if ($profile?.user?.notifications.inbox ?? 0) > 0}
-          <Badge
-            color="red-subtle"
-            class="w-5 h-5 !p-0 grid place-items-center"
-          >
-            {$profile?.user?.notifications.inbox}
-          </Badge>
-        {/if}
-      </span>
-    </SidebarButton>
-  {/if}
   {#if $profileData.profiles.length >= 1}
     <hr class="border-slate-200 dark:border-zinc-900 my-1" />
     {#each [...$profileData.profiles] as prof, index (prof.id)}
