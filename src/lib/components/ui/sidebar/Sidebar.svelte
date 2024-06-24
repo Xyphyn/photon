7d151f8ab8e96a902ab227827b7783b6bf0b6a9b
Tywele <lena.development@proton.me>
2024-06-24 15:14:46 +0200

    Added setting to toggle readded menu entries

--- src/lib/components/ui/sidebar/Sidebar.svelte
+++ src/lib/components/ui/sidebar/Sidebar.svelte
@@ -35,6 +35,7 @@
   gap-1 h-fit max-h-screen {$$props.class}"
   style={$$props.style}
 >
+{#if $userSettings.legacySidebarNavigation}
 <SidebarButton icon={Home} href="/">
   <span slot="label">
     {$t('nav.home')}
@@ -51,6 +52,7 @@
   </span>
 </SidebarButton>
 <hr class="border-slate-200 dark:border-zinc-900 my-1" />
+{/if}
   {#if $profile?.jwt}
     <SidebarButton icon={UserCircle} href="/profile/user">
       <span slot="label">
@@ -88,13 +90,15 @@
     >
       <span slot="label">{$t('account.signup')}</span>
     </SidebarButton>
-    <SidebarButton
+    {#if $userSettings.legacySidebarNavigation === false}
+      <SidebarButton
       href="/settings"
       title={$t('nav.menu.settings')}
       icon={Cog6Tooth}
-    >
-      <span slot="label">{$t('nav.menu.settings')}</span>
-    </SidebarButton>
+      >
+        <span slot="label">{$t('nav.menu.settings')}</span>
+      </SidebarButton>
+    {/if}
   {/if}
   {#if $profileData.profiles.length >= 1}
     <hr class="border-slate-200 dark:border-zinc-900 my-1" />
