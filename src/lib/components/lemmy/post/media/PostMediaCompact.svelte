df0a44b9c2efcde950c8ab8998d094c75ff28f83
sevonj <100710152+sevonj@users.noreply.github.com>
2024-07-06 15:35:04 +0300

    refactor: redundant check

--- src/lib/components/lemmy/post/media/PostMediaCompact.svelte
+++ src/lib/components/lemmy/post/media/PostMediaCompact.svelte
@@ -17,76 +17,74 @@
   export let type: MediaType = 'none'
 </script>
 
-{#if view == 'compact' || view == 'list'}
-  <div
-    class="w-24 sm:w-32 h-24 relative group/media {$$props.class ?? ''}"
-    style={$$props.style ?? ''}
-  >
-    {#if post.alt_text}
-      <Popover
-        openOnHover
-        placement={$userSettings.leftAlign ? 'bottom-start' : 'bottom-end'}
-      >
-        <Material
-          slot="target"
-          padding="none"
-          rounding="full"
-          elevation="high"
-          class="w-max absolute bottom-0 left-0 py-0.5 px-1.5 m-1 font-bold"
-        >
-          ALT
-        </Material>
-        <div class="max-w-sm">
-          {post.alt_text}
-        </div>
-      </Popover>
-    {/if}
-    <svelte:element
-      this={!$userSettings.expandImages || type != 'image' ? 'a' : 'button'}
-      href={postLink(post)}
-      on:click={() => {
-        if (type == 'image') {
-          showImage(bestImageURL(post, false, 2048))
-        }
-      }}
-      role="button"
-      tabindex="0"
+<div
+  class="w-24 sm:w-32 h-24 relative group/media {$$props.class ?? ''}"
+  style={$$props.style ?? ''}
+>
+  {#if post.alt_text}
+    <Popover
+      openOnHover
+      placement={$userSettings.leftAlign ? 'bottom-start' : 'bottom-end'}
     >
-      {#if post.thumbnail_url || isImage(post.url)}
-        <img
-          src={optimizeImageURL(post.thumbnail_url || post.url || '', 128)}
-          loading="lazy"
-          class="object-cover overflow-hidden bg-slate-100 dark:bg-zinc-800 rounded-xl h-24 w-24 sm:w-32
-          border border-slate-200 dark:border-zinc-800 group-hover/media:border-slate-400
-          group-hover/media:dark:border-zinc-600 transition-colors"
-          alt={post.name}
-        />
-        {#if type != 'image'}
-          <div
-            class="relative w-7 h-7 bottom-8 left-1 rounded-lg text-slate-800 dark:text-zinc-200
-            backdrop-blur-sm bg-[#ffffff]/75 dark:bg-black/75 grid place-items-center"
-          >
-            <Icon src={type == 'iframe' ? VideoCamera : Link} mini size="16" />
-          </div>
-        {/if}
-      {:else}
+      <Material
+        slot="target"
+        padding="none"
+        rounding="full"
+        elevation="high"
+        class="w-max absolute bottom-0 left-0 py-0.5 px-1.5 m-1 font-bold"
+      >
+        ALT
+      </Material>
+      <div class="max-w-sm">
+        {post.alt_text}
+      </div>
+    </Popover>
+  {/if}
+  <svelte:element
+    this={!$userSettings.expandImages || type != 'image' ? 'a' : 'button'}
+    href={postLink(post)}
+    on:click={() => {
+      if (type == 'image') {
+        showImage(bestImageURL(post, false, 2048))
+      }
+    }}
+    role="button"
+    tabindex="0"
+  >
+    {#if post.thumbnail_url || isImage(post.url)}
+      <img
+        src={optimizeImageURL(post.thumbnail_url || post.url || '', 128)}
+        loading="lazy"
+        class="object-cover overflow-hidden bg-slate-100 dark:bg-zinc-800 rounded-xl h-24 w-24 sm:w-32
+        border border-slate-200 dark:border-zinc-800 group-hover/media:border-slate-400
+        group-hover/media:dark:border-zinc-600 transition-colors"
+        alt={post.name}
+      />
+      {#if type != 'image'}
         <div
-          class="object-cover overflow-hidden bg-slate-100 dark:bg-zinc-800 rounded-xl h-24 w-24 sm:w-32
-          border border-slate-200 dark:border-zinc-800 group-hover/media:border-slate-400
-          group-hover/media:dark:border-zinc-600 transition-colors text-slate-400 dark:text-zinc-600 grid
-          place-items-center"
+          class="relative w-7 h-7 bottom-8 left-1 rounded-lg text-slate-800 dark:text-zinc-200
+          backdrop-blur-sm bg-[#ffffff]/75 dark:bg-black/75 grid place-items-center"
         >
-          <Icon
-            src={type == 'embed'
-              ? Link
-              : type == 'iframe'
-                ? VideoCamera
-                : DocumentText}
-            solid
-            size="32"
-          />
+          <Icon src={type == 'iframe' ? VideoCamera : Link} mini size="16" />
         </div>
       {/if}
-    </svelte:element>
-  </div>
-{/if}
+    {:else}
+      <div
+        class="object-cover overflow-hidden bg-slate-100 dark:bg-zinc-800 rounded-xl h-24 w-24 sm:w-32
+        border border-slate-200 dark:border-zinc-800 group-hover/media:border-slate-400
+        group-hover/media:dark:border-zinc-600 transition-colors text-slate-400 dark:text-zinc-600 grid
+        place-items-center"
+      >
+        <Icon
+          src={type == 'embed'
+            ? Link
+            : type == 'iframe'
+              ? VideoCamera
+              : DocumentText}
+          solid
+          size="32"
+        />
+      </div>
+    {/if}
+  </svelte:element>
+</div>
