49c7e46081db37c1e3a1501ae50f8c46973e5413
Xylight <james@xylight.dev>
2024-07-16 13:09:11 -0700

    fix: rtl layout

--- src/lib/components/input/Switch.svelte
+++ src/lib/components/input/Switch.svelte
@@ -17,7 +17,7 @@
   }
 
   let containerClass = `
-    flex flex-row items-center w-max max-w-full overflow-auto
+    flex flex-row rtl:flex-row-reverse items-center w-max max-w-full overflow-auto
     ${headless ? 'pb-1 gap-1' : ''}
     ${$$props.class}
   `
