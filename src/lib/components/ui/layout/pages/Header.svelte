b1a3ffc321e80d08e0e21e1f5cdfd54b6f44ec12
Xylight <james@xylight.dev>
2024-07-26 14:07:28 -0700

    fix: set width to full for headers

--- src/lib/components/ui/layout/pages/Header.svelte
+++ src/lib/components/ui/layout/pages/Header.svelte
@@ -5,13 +5,15 @@
 
 <div
   class="
+  w-[calc(100%+2rem)]
+  sm:w-[calc(100%+3rem)]
   bg-slate-50 dark:bg-zinc-950 -m-4 sm:-m-6 mb-0 sm:mb-0 sm:p-6 p-4
    border-b border-slate-200 dark:border-zinc-800"
   style="margin-top: min(-1.5rem, calc(-1 * calc({$contentPadding.top}px)));
 padding-top: max(1.5rem, calc({$contentPadding.top}px));"
 >
   <h1
-    class="text-3xl font-bold flex items-center gap-2
+    class="text-3xl font-bold items-center gap-2 w-full
   {$userSettings.font == 'satoshi/nunito'
       ? 'font-display'
       : ''} {$$props.class ?? ''}"
