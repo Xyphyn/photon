99caaec3e8a3a2a6117cfeacfbe0223cf964ccdd
Xylight <james@xylight.dev>
2024-07-26 14:02:34 -0700

    fix: use zinc and slate for tabs color

--- src/lib/components/ui/layout/pages/Tabs.svelte
+++ src/lib/components/ui/layout/pages/Tabs.svelte
@@ -9,21 +9,21 @@
 </script>
 
 <nav
-  class="flex flex-row items-center gap-1 p-1 rounded-full bg-[#fff]/60 dark:bg-neutral-800/60
-  backdrop-blur-lg border border-gray-200/60 dark:border-neutral-800 shadow-lg {$$props.class ??
+  class="flex flex-row items-center gap-1 p-1 rounded-full bg-white/60 dark:bg-zinc-800/60
+  backdrop-blur-lg border border-slate-200/60 dark:border-zinc-800 shadow-lg {$$props.class ??
     ''}
   "
 >
   {#each routes as route}
     <a
       href={route.href}
-      class="font-medium rounded-full px-4 py-1 hover:bg-gray-200/40 hover:dark:bg-neutral-700/40
+      class="font-medium rounded-full px-4 py-1 hover:bg-slate-200/40 hover:dark:bg-zinc-700/40
       transition-colors duration-100 relative z-0"
     >
       {route.name}
       {#if $page.url.pathname == route.href}
         <div
-          class="rounded-full bg-gray-100/60 dark:bg-neutral-700/60
+          class="rounded-full bg-slate-100/60 dark:bg-zinc-700/60
           absolute inset-0 w-full h-full -z-10"
         ></div>
       {/if}
