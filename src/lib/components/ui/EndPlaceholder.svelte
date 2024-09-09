3f15d5eb6fc16e48178d6fa0e889fa7a98fcaf2d
acidrums4 <acidrums4@gmail.com>
2024-09-08 20:16:19 -0500

    Remove redundant <hr> tags within expandable headers
    There are <hr> tags within the expandable headers, the clickable components that expand or collapse when you click on them (for example, at the left sidebar of the home page on desktop) that seem redundant to me, as the expandable headers themselves are separated with more <hr> tags - so they look a bit cluttered. I'd like if you consider removing this <hr> tags placed within this expandable headers and giving said sidebars a cleaner look.

--- src/lib/components/ui/EndPlaceholder.svelte
+++ src/lib/components/ui/EndPlaceholder.svelte
@@ -3,9 +3,8 @@
    text-sm text-slate-600 dark:text-zinc-400 gap-2 flex-wrap {$$props.class ||
     ''}"
 >
-  <span class="font-medium text-left">
+  <span class="flex-1 font-medium text-left">
     <slot />
   </span>
-  <hr class="border-slate-200 flex-1 w-full dark:border-zinc-800" />
   <slot name="action" />
 </div>
