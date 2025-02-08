216f9cb1eb65506ef9232e7a202655247ff6b50b
James Xylight <james@xylight.dev>
2025-02-08 15:40:04 -0800

    misc: tweak menubutton height

--- src/lib/mono/popover/MenuButton.svelte
+++ src/lib/mono/popover/MenuButton.svelte
@@ -36,7 +36,7 @@
   {...rest}
   {color}
   rounding="none"
-  class="w-full px-2 rounded-lg min-h-[36px] font-normal {disabled
+  class="w-full px-2 rounded-lg min-h-8 font-medium {disabled
     ? 'opacity-70 pointer-events-none cursor-not-allowed'
     : ''} {color == 'tertiary'
     ? 'hover:dark:bg-zinc-800/70'
