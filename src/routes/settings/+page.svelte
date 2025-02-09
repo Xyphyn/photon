16072c14d1f5ed1fe331aa59ef260a6514bb24e0
James Xylight <james@xylight.dev>
2025-02-08 20:37:37 -0800

    i18n: add arabic support

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -65,6 +65,7 @@
     }
   > = new Map([
     ['en', { name: 'English', translated: -1, flag: '🇬🇧' }],
+    ['ar', { name: 'العربية', translated: 0.3, flag: '🟩' }],
     ['he', { name: 'עברית', translated: -1, flag: '🇮🇱' }],
     ['bg', { name: 'български', translated: 0.67, flag: '🇧🇬' }],
     ['de', { name: 'Deutsch', translated: 0.7, flag: '🇩🇪' }],
@@ -250,7 +251,7 @@
           {/each}
         </Select>
       </Setting>
-      {#if $locale == 'he'}
+      {#if $locale == 'he' || $locale == 'ar'}
         <ToggleSetting
           bind:checked={settings.useRtl}
           title={$t('settings.app.lang.useRtl.title')}
