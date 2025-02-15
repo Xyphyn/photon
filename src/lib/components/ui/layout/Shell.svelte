70770d5872fa2a2f35b8095bef96046bb9fd2cb2
James Xylight <james@xylight.dev>
2025-02-14 18:11:37 -0800

    cleanup: remove dock options and make adaptive by default

--- src/lib/components/ui/layout/Shell.svelte
+++ src/lib/components/ui/layout/Shell.svelte
@@ -1,5 +1,5 @@
 <script lang="ts" module>
-  import { innerWidth } from 'svelte/reactivity/window'
+  import { innerWidth, scrollY } from 'svelte/reactivity/window'
 
   const calculateDockProperties = (
     settings: {
@@ -14,15 +14,9 @@
     let panel = false
     let top = false
 
-    if (screenWidth >= 1000) {
+    if (screenWidth >= 1024) {
       panel = true
       top = true
-    } else if (screenWidth > 800) {
-      panel = true
-      top = false
-    } else if (screenWidth > 600) {
-      panel = false
-      top = false
     }
 
     panel = settings.noGap ?? panel
@@ -63,8 +57,6 @@
     return { top: 0, class: ' failed-to-calculate', bottom: 0 }
   }
 
-  export let screenWidth = writable(1000)
-
   let dockProps = $derived(
     calculateDockProperties(settings.dock, innerWidth.current ?? 1000),
   )
@@ -82,10 +74,16 @@
     top: boolean
   }>(dockProps)
 
-  let sidePadding = $derived(
-    calculatePadding(dockProps.noGap, dockProps.top, false),
-  )
-  let topPanel = $derived(dockProps.noGap && dockProps.top)
+  $effect.root(() => {
+    $effect(() => {
+      dockPropsStore.set(
+        calculateDockProperties(settings.dock, innerWidth.current ?? 1000),
+      )
+      contentPaddingStore.set(
+        calculatePadding(dockProps.noGap, dockProps.top, true),
+      )
+    })
+  })
 
   export { contentPaddingStore as contentPadding, dockPropsStore as dockProps }
 </script>
@@ -94,7 +92,7 @@
   import { settings } from '$lib/settings.svelte.js'
   import { theme } from '$lib/ui/colors.svelte'
   import { writable, type Readable, type Writable } from 'svelte/store'
-  import type { ClassValue } from 'svelte/elements'
+  import type { ClassValue, UIEventHandler } from 'svelte/elements'
 
   interface Props {
     route?: { id: string | null } | undefined
@@ -118,11 +116,17 @@
     ...rest
   }: Props = $props()
 
-  $effect(() => {
-    contentPaddingStore.set(contentPadding)
-    dockPropsStore.set(dockProps)
-  })
+  let previousTop = 0
+  const onscroll: UIEventHandler<Window> = (e) => {
+    dockVisible = (scrollY?.current ?? 0) > previousTop
+
+    previousTop = scrollY?.current ?? 0
+  }
+
+  let dockVisible = $state(true)
 </script>
+
+<svelte:window {onscroll} />
 
 <div
   {...rest}
@@ -131,34 +135,18 @@
 >
   {@render children?.()}
   <div
-    class="
-    {dockProps.noGap ? '' : 'p-4 max-w-3xl left-1/2 -translate-x-1/2'}
-    {dockProps.top ? 'top-0' : 'bottom-0'}
-    {topPanel ? 'fixed top-0' : 'fixed'}
+    class="fixed lg:sticky bottom-0 {dockVisible
+      ? 'max-lg:-bottom-24'
+      : ''}  lg:top-0
+    max-w-3xl p-4 left-1/2 -translate-x-1/2 lg:max-w-full lg:p-0 lg:left-0 lg:translate-x-0
     w-full z-50 pointer-events-none"
     style="grid-area: navbar;
-    transition-property: padding, top, bottom;
-    transition-duration: 250ms;
-    transition-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);"
+    transition: bottom .4s cubic-bezier(0.075, 0.82, 0.165, 1);"
   >
     {@render navbar?.({
-      class: `
-      ${
-        dockProps.noGap
-          ? dockProps.top
-            ? 'border-b shadow-none rounded-none'
-            : 'border-t rounded-none'
-          : 'border rounded-full'
-      }
-       
-       dark:bg-transparent transition-colors duration-500
-      pointer-events-auto backdrop-blur-xl ${
-        topPanel
-          ? 'bg-slate-50/50 dark:bg-zinc-950/90 border-slate-100 dark:border-zinc-900'
-          : `border-slate-200 dark:border-zinc-800 shadow-2xl
-        bg-white/50 dark:bg-zinc-950/70`
-      }`,
-      style: 'transition: border-radius 250ms;',
+      class: `bg-slate-50/50 dark:bg-zinc-950/90 border-slate-100 dark:border-zinc-900
+      pointer-events-auto backdrop-blur-xl border lg:border-t-0 lg:border-x-0
+      rounded-full lg:rounded-none`,
     })}
   </div>
   <div
@@ -168,17 +156,16 @@
   >
     {@render sidebar?.({
       class: `hidden md:flex sticky top-0 left-0 h-max bg-slate-50 dark:bg-zinc-950
-      z-40
-      ${sidePadding.class}`,
+      z-40 lg:top-16`,
       style: 'grid-area: sidebar; width: 100% !important;',
     })}
     {@render main?.({
-      class: `w-full bg-slate-25 dark:bg-zinc-925 justify-self-center shadow-sm z-0
-      ${contentPadding.class} main`,
+      class: `w-full bg-slate-25 dark:bg-zinc-925 justify-self-center shadow-sm z-0 main`,
       style: 'grid-area: main',
     })}
     {@render suffix?.({
-      class: `max-xl:hidden w-full sticky top-0 left-0 max-h-screen bg-slate-50 dark:bg-zinc-950 z-40 ${sidePadding.class}`,
+      class: `max-xl:hidden w-full sticky top-0 left-0 max-h-screen bg-slate-50 dark:bg-zinc-950
+      z-40 lg:top-16`,
       style: 'grid-area: suffix;',
     })}
   </div>
