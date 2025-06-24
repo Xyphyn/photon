eb6d08943c82e5ea7cd595fac63aa17767e8a908
James Xylight <james@xylight.dev>
2025-06-24 16:50:55 -0700

    feat: fix posts/comments

--- src/lib/client/piefed.svelte.ts
+++ src/lib/client/piefed.svelte.ts
@@ -1,42 +1,71 @@
+// eslint-disable-next-line
+// @ts-nocheck
 import {
   LemmyHttp,
   type GetPosts,
   type GetPostsResponse,
   type GetSiteResponse,
   type Person,
+  type Post,
 } from 'lemmy-js-client'
 
 export class PiefedHttp extends LemmyHttp {
-  // /**
-  //  *
-  //  * @param person A piefed person object to rewrite
-  //  */
-  // private rewritePerson(person: Person): Person {
+  /**
+   *
+   * @param person A piefed person object to rewrite
+   */
+  private static rewritePerson(person: Person): Person {
+    return {
+      ...person,
+      display_name: person.title,
+      name: person.user_name,
+    }
+  }
 
-  // }
+  private static rewritePost(post: Post): Post {
+    return {
+      ...post,
+      name: post.title,
+      featured_community: post.stickied,
+      updated: post.edited_at,
+    }
+  }
 
   override async getSite(): Promise<GetSiteResponse> {
     const res = await super.getSite()
-
-    console.log(res)
 
     return {
       ...res,
       site_view: {
         site: {
-          // @ts-expect-error api difference
           ...res.site,
         },
         local_site: {
-          // @ts-expect-error api difference
           downvotes_enabled: res.site.enable_downvotes,
+        },
+      },
+      my_user: {
+        ...res.my_user,
+        local_user_view: {
+          ...res.my_user?.local_user_view,
+          person: PiefedHttp.rewritePerson(res.my_user?.local_user_view.person),
         },
       },
     } as unknown as GetSiteResponse
   }
 
-  // override async getPosts(params: GetPosts): Promise<GetPostsResponse> {
-  //   const res = await super.getPosts(params)
+  override async getPosts(params: GetPosts): Promise<GetPostsResponse> {
+    const res = await super.getPosts(params)
 
-  // }
+    return {
+      ...res,
+      posts: res.posts.map(post => {
+        return {
+          ...post,
+          post: PiefedHttp.rewritePost(post.post),
+          creator: PiefedHttp.rewritePerson(post.creator),
+        }
+      }),
+    }
+  }
 }
