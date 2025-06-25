23fd09029bd0f961d894ef09774afa9a8706814a
James Xylight <james@xylight.dev>
2025-06-25 16:56:01 -0700

    fix: use reactive state for pictrs media, close #553

--- src/routes/admin/media/+page.svelte
+++ src/routes/admin/media/+page.svelte
@@ -12,14 +12,14 @@
 
 <Header pageHeader>{$t('routes.profile.media.title')}</Header>
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-  {#each data.images as image (image.local_image.pictrs_delete_token)}
+  {#each data.images.value as image (image.local_image.pictrs_delete_token)}
     <div animate:flip={{ duration: 500, easing: expoInOut }}>
       <PictrsImage
         image={image.local_image}
         user={image.person}
         ondelete={() => {
-          data.images = data.images.toSpliced(
-            data.images.findIndex(
+          data.images.value = data.images.value.toSpliced(
+            data.images.value.findIndex(
               i =>
                 i.local_image.pictrs_delete_token ==
                 image.local_image.pictrs_delete_token,
@@ -31,7 +31,7 @@
     </div>
   {/each}
 </div>
-{#if data.images.length == 20}
+{#if data.images.value.length == 20}
   <Pageination
     page={Number(page.url.searchParams.get('page')) || 1}
     href={page => `?page=${page}`}
--- src/routes/profile/media/+page.svelte
+++ src/routes/profile/media/+page.svelte
@@ -12,13 +12,13 @@
 
 <Header pageHeader>{$t('routes.admin.media.title')}</Header>
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-  {#each data.images as image (image.local_image.pictrs_delete_token)}
+  {#each data.images.value as image (image.local_image.pictrs_delete_token)}
     <div animate:flip={{ duration: 500, easing: expoInOut }}>
       <PictrsImage
         image={image.local_image}
         ondelete={() => {
-          data.images = data.images.toSpliced(
-            data.images.findIndex(
+          data.images.value = data.images.value.toSpliced(
+            data.images.value.findIndex(
               i =>
                 i.local_image.pictrs_delete_token ==
                 image.local_image.pictrs_delete_token,
@@ -30,7 +30,7 @@
     </div>
   {/each}
 </div>
-{#if data.images.length == 20}
+{#if data.images.value.length == 20}
   <Pageination
     page={Number(page.url.searchParams.get('page')) || 1}
     href={page => `?page=${page}`}
