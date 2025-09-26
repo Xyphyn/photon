f54e6242b3af25bfee035100b5b57855c04523a3
Xylight <james@xylight.dev>
2025-09-25 21:32:04 -0700

    fix: update errorMessage to support lemmy errors again

--- src/lib/app/error.ts
+++ src/lib/app/error.ts
@@ -1,6 +1,16 @@
+import { t } from "./i18n"
+
 // eslint-disable-next-line
 export function errorMessage(error: any, instance?: string): string {
   try {
+    if (typeof error == 'string') {
+      try {
+        error = JSON.parse(error)
+      } catch { 
+        /* try without catch would be cool imo */
+      }
+    }
+
     if (error?.body?.message) {
       error = JSON.parse(error?.body?.message).error
     } else if (error?.message) {
