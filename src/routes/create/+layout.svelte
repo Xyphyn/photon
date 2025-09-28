3fda9d9f2e92f9b291f251a7601f470ea1bc1983
Xylight <james@xylight.dev>
2025-09-28 13:54:47 -0700

    fix: use new admin/mod getters for post page, close #663

--- src/routes/create/+layout.svelte
+++ src/routes/create/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { site } from '$lib/api/client.svelte'
+  import { profile } from '$lib/app/auth.svelte'
   import { t } from '$lib/app/i18n'
   import { Header, Tabs } from '$lib/ui/layout'
 
@@ -9,7 +10,7 @@
 <Header pageHeader>
   {$t('nav.create.label')}
   {#snippet extended()}
-    {#if !site || !site.data?.site_view.local_site.community_creation_admin_only}
+    {#if !site || !(site.data?.site_view.local_site.community_creation_admin_only && !profile.isAdmin)}
       <Tabs
         margin={false}
         style="subpage"
