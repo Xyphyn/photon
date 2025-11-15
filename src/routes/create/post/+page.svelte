5f5ee46ec430dbc624f9b781ffc1208a4ece508f
Xylight <james@xylight.dev>
2025-11-15 15:06:15 -0800

    fix: use base community as PostFormState

--- src/routes/create/post/+page.svelte
+++ src/routes/create/post/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { goto } from '$app/navigation'
   import { page } from '$app/state'
+  import type { CommunityView } from '$lib/api/types'
   import { t } from '$lib/app/i18n'
   import { getSessionStorage, setSessionStorage } from '$lib/app/session'
   import PostForm from '$lib/feature/post/form/PostForm.svelte'
@@ -11,10 +12,8 @@
   import { postLink } from '$lib/feature/post/helpers.js'
   import { onDestroy } from 'svelte'
 
-  let { data } = $props()
-
   let community = getSessionStorage('lastSeenCommunity') as
-    | { id: number; name: string }
+    | CommunityView
     | undefined
 
   onDestroy(() => {
@@ -22,9 +21,14 @@
   })
 
   let crosspost = $derived.by<PostFormInit>(() => {
-    return JSON.parse(
-      atob(page.url.searchParams.get('crosspost') ?? '') || '{}',
-    )
+    const crosspostParam = page.url.searchParams.get('crosspost')
+    try {
+      return crosspostParam
+        ? JSON.parse(atob(crosspostParam) || '{}')
+        : undefined
+    } catch {
+      return undefined
+    }
   })
 </script>
 
@@ -33,27 +37,8 @@
 </svelte:head>
 
 <PostForm
-  init={new PostFormState(crosspost)}
+  init={new PostFormState(crosspost ?? { community: community?.community })}
   onsubmit={(post) => goto(postLink(post.post))}
 >
   {#snippet title()}{/snippet}
 </PostForm>
-
-<!-- <PostFormOld
-  passedData={data.crosspost == true
-    ? post
-    : {
-        body: '',
-        community: null,
-        image: null,
-        loading: false,
-        nsfw: false,
-        title: '',
-        url: undefined,
-        flair_list: [],
-      }}
-  passedCommunity={community}
-  onsubmit={(e) => goto(`/post/${e.post.id}`)}
->
-  {#snippet formtitle()}{/snippet}
-</PostFormOld> -->
