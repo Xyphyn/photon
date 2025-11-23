22a1b6fe7b306d35c922d19c8200bce564b13c50
Xylight <james@xylight.dev>
2025-11-22 21:35:09 -0800

    feat: add tauri android build

---
+++ src-tauri/gen/android/app/src/main/java/app/phtn/client/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.phtn.client
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,26 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [sveltekit(), tailwindcss()],
+  plugins: [
+    // remove +page.server.ts on tauri to prevent __data.json dependencies
+    ...(!!process.env.TAURI_ENV_PLATFORM
+      ? [
+          {
+            name: 'ignore-server-files',
+            load(id: string) {
+              if (
+                id.includes('+page.server') ||
+                id.includes('+layout.server')
+              ) {
+                return 'export const load = undefined'
+              }
+            },
+          },
+        ]
+      : []),
+    sveltekit(),
+    tailwindcss(),
+  ],
   build: {
     sourcemap: true,
   },
