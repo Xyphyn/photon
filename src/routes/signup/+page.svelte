ec2238718a6478576f72a4f71ff062c8fccb7d8a
Xylight <james@xylight.dev>
2026-02-13 20:45:52 -0800

    design: make signup page look slightly less awful (allow commonlist in virtualizer)

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -20,7 +20,7 @@
   import { DEFAULT_INSTANCE_URL } from '$lib/app/instance.svelte'
   import VirtualList from '$lib/app/render/VirtualList.svelte'
   import Avatar from '$lib/ui/generic/Avatar.svelte'
-  import { Header } from '$lib/ui/layout'
+  import { CommonList, Header } from '$lib/ui/layout'
   import { Button, TextInput, toast } from 'mono-svelte'
   import { onMount } from 'svelte'
   import { ArrowLeft, Icon } from 'svelte-hero-icons/dist'
@@ -35,8 +35,8 @@
   onMount(async () => {
     try {
       const res: Instance[] = (
-        await fetch(`https://data.lemmyverse.net/data/instance.full.json`).then(
-          (r) => r.json(),
+        await fetch(`https://servers.infra.phtn.app?limit=30`).then((r) =>
+          r.json(),
         )
       )
         .filter((i: Instance) => i.fed)
@@ -72,44 +72,50 @@
   </Button>
   <Header>{$t('form.signup.title')}</Header>
   <p>{$t('form.signup.description')}</p>
-  <p>
-    {$t('form.signup.info')}
-  </p>
   {#if instances}
-    <VirtualList
-      items={instances}
-      useWindow={false}
-      height={400}
-      estimatedHeight={50}
-      class="overflow-auto border rounded-2xl border-slate-200 dark:border-zinc-900 bg-white dark:bg-zinc-950
-      divide-y divide-slate-200 dark:divide-zinc-900"
-    >
-      {#snippet item(i)}
-        <button
-          onclick={() => (selectedInstance = instances![i].baseurl ?? '')}
-          class="flex flex-row gap-2 text-left py-2 first:pt-0 last:pb-0 items-center
-            h-16 max-h-16 min-h-16 overflow-hidden w-full cursor-pointer hover:bg-slate-50 hover:dark:bg-zinc-925 duration-100
-            p-4"
-        >
-          <Avatar
-            width={32}
-            url={instances![i].icon}
-            alt={instances![i].name}
-            class="shrink-0"
-          />
-          <div class="flex flex-col max-h-full w-full">
-            <span class="font-medium text-base whitespace-nowrap text-ellipsis">
-              {instances![i].name}
-            </span>
-            <span
-              class="whitespace-nowrap text-ellipsis overflow-hidden w-full"
-            >
-              {instances![i].desc}
-            </span>
-          </div>
-        </button>
-      {/snippet}
-    </VirtualList>
+    <CommonList>
+      <VirtualList
+        items={instances}
+        useWindow={false}
+        height={384}
+        estimatedHeight={50}
+        class="overflow-auto overflow-x-hidden w-full min-w-0 min-h-96"
+        itemContainer="li"
+      >
+        {#snippet item(i)}
+          {@const instance = instances![i]}
+          <button
+            onclick={() => (selectedInstance = instance.baseurl ?? '')}
+            class="flex flex-row items-center text-left gap-2 w-full cursor-pointer"
+          >
+            <Avatar
+              width={32}
+              url={instance.icon}
+              alt={instance.name}
+              class="shrink-0"
+            />
+            <div class="flex flex-col max-h-full w-full">
+              <div
+                class="font-medium text-base whitespace-nowrap text-ellipsis flex"
+              >
+                <span>
+                  {instance.name}
+                </span>
+                <span class="text-slate-500 dark:text-zinc-500 ml-auto">
+                  {instance.baseurl}
+                </span>
+              </div>
+
+              <p
+                class="overflow-hidden overflow-ellipsis w-full text-sm text-slate-600 dark:text-zinc-400"
+              >
+                {instance.desc}
+              </p>
+            </div>
+          </button>
+        {/snippet}
+      </VirtualList>
+    </CommonList>
   {/if}
   <form
     class="flex flex-col gap-4"
